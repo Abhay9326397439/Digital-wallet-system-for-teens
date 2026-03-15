578e553152c50c88bf048313fdba8921c987c220
Abhay9326397439 <abhayzombade26@gmail.com>
2026-03-15 20:11:31 +0530

    added userSettings

--- TeenWalletManager/src/main/java/com/teenwallet/model/UserSettings.java
+++ TeenWalletManager/src/main/java/com/teenwallet/model/UserSettings.java
@@ -4,51 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UserSettings {
+    private String username;
     private double dailyLimit;
     private double weeklyLimit;
     private boolean cardLocked;
     private Map<String, Double> categoryLimits;
 
-    public UserSettings() {
+    public UserSettings(String username) {
+        this.username = username;
         this.dailyLimit = 300.0;
         this.weeklyLimit = 1500.0;
         this.cardLocked = false;
         this.categoryLimits = new HashMap<>();
-
-        // Default category limits
-        categoryLimits.put("Food", 100.0);
-        categoryLimits.put("Entertainment", 150.0);
-        categoryLimits.put("Education", 200.0);
-        categoryLimits.put("Shopping", 200.0);
-        categoryLimits.put("Transport", 100.0);
-        categoryLimits.put("Others", 100.0);
     }
 
-    public String toFileString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(dailyLimit).append("|").append(weeklyLimit).append("|").append(cardLocked);
-        for (Map.Entry<String, Double> entry : categoryLimits.entrySet()) {
-            sb.append("|").append(entry.getKey()).append(":").append(entry.getValue());
-        }
-        return sb.toString();
-    }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-    public static UserSettings fromFileString(String line) {
-        UserSettings settings = new UserSettings();
-        String[] parts = line.split("\\|");
-        settings.dailyLimit = Double.parseDouble(parts[0]);
-        settings.weeklyLimit = Double.parseDouble(parts[1]);
-        settings.cardLocked = Boolean.parseBoolean(parts[2]);
-
-        settings.categoryLimits.clear();
-        for (int i = 3; i < parts.length; i++) {
-            String[] limitParts = parts[i].split(":");
-            settings.categoryLimits.put(limitParts[0], Double.parseDouble(limitParts[1]));
-        }
-        return settings;
-    }
-
-    // Getters and setters
     public double getDailyLimit() { return dailyLimit; }
     public void setDailyLimit(double dailyLimit) { this.dailyLimit = dailyLimit; }
 
